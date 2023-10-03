49996f1f3e6f45be883f335f99393e30b40eeb0e
vlad <vnelipovich63@gmail.com>
2023-10-03 15:06:22 +0300

    did minimal structure and the ability to receive data

---
+++ news-tracker/src/News.tsx
@@ -0,0 +1,50 @@
+import React, {useState} from 'react';
+import styles from "./App.module.scss"
+
+interface INewsData {
+    author: string;
+    title: string;
+    description: string;
+    urlToImage: string
+}
+
+const News: React.FC = () => {
+    const [newsData, setNews] = useState<INewsData[]>([]);
+    const COUNTRY = 'ru';
+    const API_KEY = "9104cee86a3240cbb4f97d269814257d";
+    const URL = `https://newsapi.org/v2/top-headlines?country=${COUNTRY}&apiKey=${API_KEY}`;
+
+    const getData = async () => {
+        try {
+            const response = await fetch(URL);
+            const data = await response.json();
+            setNews(data.articles);
+        } catch (error) {
+            console.log('Ошибка при выполнении GET-запроса:', error);
+        }
+    };
+
+    return (
+        <div className={styles.NewsBlock}>
+            <button className={styles.getDataBtn} onClick={getData}>Получить новости</button>
+            <div>
+                {newsData.length === 0 ? (
+                    <div>Загрузка...</div>
+                ) : (
+                    newsData.map((news) => (
+                        <div className={styles.newsContainer} key={news.title}>
+
+                            <div>{news.title}</div>
+                            <div><img width={60} src={news.urlToImage} alt={news.title}/></div>
+                            <div>{news.description}</div>
+                            <div>{news.author}</div>
+
+                        </div>
+                    ))
+                )}
+            </div>
+        </div>
+    );
+};
+
+export default News;
