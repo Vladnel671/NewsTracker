e40a507472c3ad115b8384279ddb8a944a98ca7e
vlad <vnelipovich63@gmail.com>
2023-11-09 08:24:47 +0300

    did main news block structure

--- news-tracker/src/pages/MainNewsBlock.tsx
+++ news-tracker/src/pages/MainNewsBlock.tsx
@@ -24,8 +24,43 @@ const MainNewsBlock: React.FC = () => {
                     </div>
                 </div>
             </div>
-            <div className={styles.secondBlock}>Second Column</div>
-            <div className={styles.thirdBlock}>Third Column</div>
+            <div className={styles.secondBlock}>
+                <div className={styles.centralNewsBlock}>
+                    <div className={styles.centralImg}>ImgBlock2</div>
+                    <span className={styles.centralNewsAuthor}>Author</span>
+                    <span className={styles.centralText}>Text news2</span>
+                </div>
+                <div className={styles.centralListNewsBlock}>
+                    <div className={styles.firstCentralListItemBlock}>
+                        <div className={styles.firstCentralListItemImgBlock}>ImgBlock1</div>
+                        <span className={styles.firstCentralListItemTextBlock}>Text news1</span>
+                    </div>
+                    <div className={styles.secondCentralListItemBlock}>
+                        <div className={styles.secondCentralListItemImgBlock}>ImgBlock2</div>
+                        <span className={styles.secondCentralListItemTextBlock}>Text news2</span>
+                    </div>
+                </div>
+            </div>
+            <div className={styles.thirdBlock}>
+                <div className={styles.thirdColumnMainNewsBlock}>
+                    <div className={styles.thirdColumnMainNewsImg}>ImgBlock</div>
+                    <span className={styles.thirdColumnMainNewsText}>Text news</span>
+                </div>
+                <div className={styles.thirdListNewsBlock}>
+                    <div className={styles.thirdListFirstItemBlock}>
+                        <div>ImgBlock2</div>
+                        <span>Text news2</span>
+                    </div>
+                    <div className={styles.thirdListSecondItemBlock}>
+                        <div>ImgBlock3</div>
+                        <span>Text news3</span>
+                    </div>
+                    <div className={styles.thirdListThirdItemBlock}>
+                        <div>ImgBlock4</div>
+                        <span>Text news4</span>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
