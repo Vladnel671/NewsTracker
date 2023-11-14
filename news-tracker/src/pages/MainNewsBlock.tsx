d7942bbb3a28ba235d51a8a16a16c9cba579d3b2
vlad <vnelipovich63@gmail.com>
2023-11-14 21:35:53 +0300

    fixed layout issues

--- news-tracker/src/pages/MainNewsBlock.tsx
+++ news-tracker/src/pages/MainNewsBlock.tsx
@@ -11,33 +11,32 @@ const MainNewsBlock: React.FC<{ news: INewsData[] }> = ({news}) => {
 
     return (
         <div className={styles.topHeadlinesNewsBlock}>
-            <div className={styles.firstBlock}>
-                <div className={styles.firstColumnMainNewsBlock}>
+            <div className={styles.sideBlock}>
+                <div className={styles.sideColumnMainNewsBlock}>
                     <LazyImage src={firstColumnNews[0].urlToImage} alt={firstColumnNews[0].title}
-                               className={styles.firstColumnMainNewsImg}/>
-                    <span className={styles.firstColumnMainNewsText}>{firstColumnNews[0].title}</span>
+                               className={styles.sideColumnMainNewsImg}/>
+                    <span className={styles.sideColumnMainNewsText}>{firstColumnNews[0].title}</span>
                 </div>
-                <div className={styles.firstListNewsBlock}>
-                    <>
-                        <div className={styles.firstItemBlock}>
-                            <LazyImage src={firstListNewsBlock[0].urlToImage} alt={firstListNewsBlock[0].title}
-                                       className={styles.listImg}/>
-                            <span>{firstListNewsBlock[0].title}</span>
-                        </div>
-                        <div className={styles.secondItemBlock}>
-                            <LazyImage src={firstListNewsBlock[1].urlToImage} alt={firstListNewsBlock[1].title}
-                                       className={styles.listImg}/>
-                            <span>{firstListNewsBlock[1].title}</span>
-                        </div>
-                        <div className={styles.thirdItemBlock}>
-                            <LazyImage src={firstListNewsBlock[1].urlToImage} alt={firstListNewsBlock[2].title}
-                                       className={styles.listImg}/>
-                            <span>{firstListNewsBlock[2].title}</span>
-                        </div>
-                    </>
+                <div className={styles.sideListNewsBlock}>
+
+                    <div className={styles.ListItemBlock}>
+                        <LazyImage src={firstListNewsBlock[0].urlToImage} alt={firstListNewsBlock[0].title}
+                                   className={styles.listImg}/>
+                        <span>{firstListNewsBlock[0].title}</span>
+                    </div>
+                    <div className={styles.ListItemBlock}>
+                        <LazyImage src={firstListNewsBlock[1].urlToImage} alt={firstListNewsBlock[1].title}
+                                   className={styles.listImg}/>
+                        <span>{firstListNewsBlock[1].title}</span>
+                    </div>
+                    <div className={styles.ListItemBlock}>
+                        <LazyImage src={firstListNewsBlock[1].urlToImage} alt={firstListNewsBlock[2].title}
+                                   className={styles.listImg}/>
+                        <span>{firstListNewsBlock[2].title}</span>
+                    </div>
                 </div>
             </div>
-            <div className={styles.secondBlock}>
+            <div className={styles.centralBlock}>
                 <div className={styles.centralNewsBlock}>
                     <LazyImage src={thirdColumnNews[1].urlToImage} alt={thirdColumnNews[1].title}
                                className={styles.centralImg}/>
@@ -46,32 +45,38 @@ const MainNewsBlock: React.FC<{ news: INewsData[] }> = ({news}) => {
                 </div>
                 <div className={styles.centralListNewsBlock}>
                     <div className={styles.firstCentralListItemBlock}>
-                        <div className={styles.firstCentralListItemImgBlock}>ImgBlock1</div>
-                        <span className={styles.firstCentralListItemTextBlock}>Text news1</span>
+                        <LazyImage src={firstListNewsBlock[0].urlToImage} alt={firstListNewsBlock[1].title}
+                                   className={styles.firstCentralListItemImgBlock}/>
+                        <span className={styles.firstCentralListItemTextBlock}>{thirdColumnNews[1].title}</span>
                     </div>
                     <div className={styles.secondCentralListItemBlock}>
-                        <div className={styles.secondCentralListItemImgBlock}>ImgBlock2</div>
-                        <span className={styles.secondCentralListItemTextBlock}>Text news2</span>
+                        <LazyImage src={firstListNewsBlock[1].urlToImage} alt={firstListNewsBlock[1].title}
+                                   className={styles.secondCentralListItemImgBlock}/>
+                        <span className={styles.secondCentralListItemTextBlock}>{thirdColumnNews[1].title}</span>
                     </div>
                 </div>
             </div>
-            <div className={styles.thirdBlock}>
-                <div className={styles.thirdColumnMainNewsBlock}>
-                    <div className={styles.thirdColumnMainNewsImg}>ImgBlock</div>
-                    <span className={styles.thirdColumnMainNewsText}>Text news</span>
+            <div className={styles.sideBlock}>
+                <div className={styles.sideColumnMainNewsBlock}>
+                    <LazyImage src={thirdColumnNews[0].urlToImage} alt={firstListNewsBlock[0].title}
+                               className={styles.sideColumnMainNewsImg}/>
+                    <span className={styles.sideColumnMainNewsText}>{thirdColumnNews[0].title}</span>
                 </div>
-                <div className={styles.thirdListNewsBlock}>
-                    <div className={styles.thirdListFirstItemBlock}>
-                        <div>ImgBlock2</div>
-                        <span>Text news2</span>
+                <div className={styles.sideListNewsBlock}>
+                    <div className={styles.ListItemBlock}>
+                        <LazyImage src={thirdColumnNews[1].urlToImage} alt={firstListNewsBlock[1].title}
+                                   className={styles.listImg}/>
+                        <span className={styles.listText}>{thirdColumnNews[1].title}</span>
                     </div>
-                    <div className={styles.thirdListSecondItemBlock}>
-                        <div>ImgBlock3</div>
-                        <span>Text news3</span>
+                    <div className={styles.ListItemBlock}>
+                        <LazyImage src={thirdColumnNews[2].urlToImage} alt={firstListNewsBlock[2].title}
+                                   className={styles.listImg}/>
+                        <span className={styles.listText}>{thirdColumnNews[2].title}</span>
                     </div>
-                    <div className={styles.thirdListThirdItemBlock}>
-                        <div>ImgBlock4</div>
-                        <span>Text news4</span>
+                    <div className={styles.ListItemBlock}>
+                        <LazyImage src={thirdColumnNews[1].urlToImage} alt={firstListNewsBlock[1].title}
+                                   className={styles.listImg}/>
+                        <span className={styles.listText}>{thirdColumnNews[1].title}</span>
                     </div>
                 </div>
             </div>
