3ff1d53ac4c9684f8ad015593026bc1313cec3ae
vlad <vnelipovich63@gmail.com>
2023-12-01 19:25:00 +0300

    Extracted fetchNewsByCategory function

--- news-tracker/src/components/BurgerMenu.tsx
+++ news-tracker/src/components/BurgerMenu.tsx
@@ -2,45 +2,28 @@ import React, {useState} from 'react'
 import {IconButton, Menu, MenuItem} from "@material-ui/core"
 import MenuIcon from "@material-ui/icons/Menu"
 import styles from '../../src/styles/main.module.scss'
-import {fetchNewsData} from "../utils/NewsUtils.ts"
-import {setLoadingNews, setNews} from "../store/actions.ts"
 import {useDispatch} from "react-redux"
 import {useNavigate} from "react-router-dom"
-import {ALL_NEWS_URL, API_KEY} from "../constant"
+import {fetchNewsByCategory} from "../utils/NewsUtils.ts"
 
 const BurgerMenu: React.FC = () => {
     const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null)
     const dispatch = useDispatch()
     const navigate = useNavigate()
 
-    const fetchNewsByCategory = async (category: string) => {
-        const URL = `${ALL_NEWS_URL}${API_KEY}&q=${category}`
-        try {
-            dispatch(setLoadingNews(true))
-            const filteredNews = await fetchNewsData(URL)
-            dispatch(setNews(filteredNews))
-            setTimeout(() => {
-                dispatch(setLoadingNews(false))
-            }, 3000);
-            navigate('/allnews')
-        } catch (error) {
-            console.log(error);
-            dispatch(setLoadingNews(false))
-        }
-    }
-
     const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
         setAnchorEl(event.currentTarget)
     }
 
     const handleClose = () => {
-        setAnchorEl(null)
+        setAnchorEl(null);
     }
 
     const handleMenuItemClick = (category: string) => {
         handleClose()
-        fetchNewsByCategory(category)
+        fetchNewsByCategory(category, dispatch, navigate)
     }
+
     return (
         <div className={styles.BurgerMenuBlock}>
             <IconButton edge="start" color="inherit" aria-label="menu" onClick={handleClick}>
@@ -66,7 +49,7 @@ const BurgerMenu: React.FC = () => {
                 <MenuItem onClick={() => handleMenuItemClick('Sports')}>Sports</MenuItem>
             </Menu>
         </div>
-    );
-};
+    )
+}
 
-export default BurgerMenu;
+export default BurgerMenu
--- news-tracker/src/utils/NewsUtils.ts
+++ news-tracker/src/utils/NewsUtils.ts
@@ -2,9 +2,9 @@ import axios from "axios";
 import {INewsData} from "../types/types.ts"
 import {useCallback} from 'react'
 import {useDispatch, useSelector} from "react-redux"
-import {setLoadingTopHeadlines, setTopHeadlines} from "../store/actions.ts"
+import {setLoadingNews, setLoadingTopHeadlines, setNews, setTopHeadlines} from "../store/actions.ts"
 import {RootState} from "../types/types.ts"
-import {TOP_HEADLINES_URL} from "../constant"
+import {ALL_NEWS_URL, API_KEY, TOP_HEADLINES_URL} from "../constant"
 
 export const fetchNewsData = async (url: string): Promise<INewsData[]> => {
     try {
@@ -12,7 +12,7 @@ export const fetchNewsData = async (url: string): Promise<INewsData[]> => {
         const data = response.data
         return data.articles.filter((news: INewsData) => {
             return !(news.title === '[Removed]' || news.title.trim() === '')
-        });
+        })
     } catch (error) {
         console.log('Ошибка при выполнении GET-запроса:', error)
         throw error
@@ -37,5 +37,19 @@ export const fetchTopHeadlinesData = () => {
         } finally {
             dispatch(setLoadingTopHeadlines(false))
         }
-    }, [dispatch, news]);
+    }, [dispatch, news])
+}
+
+export const fetchNewsByCategory = async (category: string, dispatch: any, navigate: any) => {
+    const URL = `${ALL_NEWS_URL}${API_KEY}&q=${category}`;
+    try {
+        dispatch(setLoadingNews(true));
+        const filteredNews = await fetchNewsData(URL);
+        dispatch(setNews(filteredNews));
+        dispatch(setLoadingNews(false));
+        navigate('/allnews');
+    } catch (error) {
+        console.log(error);
+        dispatch(setLoadingNews(false));
+    }
 }
