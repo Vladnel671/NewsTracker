8e12d07c248aa84fc932412c1a23f2576072a3c7
vlad <vnelipovich63@gmail.com>
2024-01-04 17:51:21 +0300

    fix: fixed routes and project name

--- news-tracker/src/store/store.ts
+++ news-tracker/src/store/store.ts
@@ -3,6 +3,6 @@ import {configureStore} from '@reduxjs/toolkit';
 export const store = configureStore({
     reducer: {
         news: newsReducer,
-    }
+    },
 })
 export type RootState = ReturnType<typeof store.getState>
