56c053e188fe2314e452150895049dafda127739
vlad <vnelipovich63@gmail.com>
2024-01-06 17:36:09 +0300

    feat: added gh-pages

--- news-tracker/src/routes/AppRoutes.tsx
+++ news-tracker/src/routes/AppRoutes.tsx
@@ -6,7 +6,7 @@ import NotFound from "../pages/NotFound.tsx";
 export const AppRoutes = () => {
     return (
         <Routes>
-            <Route path='/' element={<TopHeadlines/>}/>
+            <Route path='/NewsTracker' element={<TopHeadlines/>}/>
             <Route path='/allnews' element={<AllNews/>}/>
             <Route path='*' element={<NotFound/>}/>
         </Routes>
