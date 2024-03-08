046bc9c998b7e9109ba0c0a68beffd5071bc2c59
vlad <vnelipovich63@gmail.com>
2024-03-09 02:37:50 +0300

    feat: add code splitting

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,15 +1,20 @@
+import { lazy, Suspense } from 'react'
 import { Route, Routes } from 'react-router-dom'
 
-import NotFound from '../components/NotFound.tsx'
-import AllNews from '../pages/AllNews.tsx'
-import TopHeadlines from '../pages/TopHeadlines.tsx'
+import Loader from '../components/ui/shared/Loader.tsx'
+
+const NotFound = lazy(() => import('../components/NotFound.tsx'))
+const AllNews = lazy(() => import('../pages/AllNews.tsx'))
+const TopHeadlines = lazy(() => import('../pages/TopHeadlines.tsx'))
 
 export const AppRoutes = () => {
   return (
-    <Routes>
-      <Route path="/" element={<TopHeadlines />} />
-      <Route path="/allnews" element={<AllNews />} />
-      <Route path="*" element={<NotFound />} />
-    </Routes>
+    <Suspense fallback={<Loader />}>
+      <Routes>
+        <Route path="/" element={<TopHeadlines />} />
+        <Route path="/allnews" element={<AllNews />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </Suspense>
   )
 }
