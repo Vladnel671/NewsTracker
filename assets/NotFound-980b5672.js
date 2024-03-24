8c81a09b1fa2fa0ba938ecc654addea59f470363
vlad <vnelipovich63@gmail.com>
2024-03-24 20:47:24 +0300

    Updates

---
+++ assets/NotFound-980b5672.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-e7a8b268.js";const o=()=>t.jsx("div",{style:{background:"#101418",display:"flex",height:"calc(100vh - 60px)",flexDirection:"row",justifyContent:"center",paddingTop:"30px",alignItems:"center",color:"white"},children:t.jsx("h1",{children:"404 - Not Found"})});export{o as default};
